1a6b5f34907c9dcfa03c6c770287bd4d2b441e45
mohit <mohitondocs@gmail.com>
2023-09-01 18:00:15 +0530

    hii

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -107,57 +107,25 @@
             }
         }
 
+        .navv {
+            width: 100%;
+        }
 
-
-
-        /* ------------------------ */
-        /* .loader {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background: #ffffff;
-            transition: opacity 1s, visibility 1s;
+        @media screen and (max-width: 425px) {
+            .navv {
+                width: 100%;
+            }
         }
         
-        .loader--hidden {
-            opacity: 0;
-            visibility: hidden;
-        }
-        
-        .loader::after {
-            content: "";
-            width: 75px;
-            height: 75px;
-            border: 15px solid #dddddd;
-            border-top-color: #009578;
-            border-radius: 50%;
-            animation: loading 5s ease infinite;
-        }        
-        
-        @keyframes loading {
-            from {
-                transform: rotate(0turn);
-            }
-
-            to {
-                transform: rotate(1turn);
-            }
-        } */
     </style>
 </head>
 
 <body style="background-color:#fffffc;">
+    
 
-    {{-- <div class="loader"></div> --}}
-
-    <div class="container" style="width: 48%;">
+    <div class="container" id="navv">
         <nav class="navbar navbar-expand-sm">
-            <div class="container">
+            <div class="container-fluid">
 
                 <a class="navbar-brand" href="{{ url('/') }}" style="font-weight:600;font-size: 1.5rem;">CandY</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
@@ -251,13 +219,7 @@
 </body>
 @yield('customjs')
 <script>
-    window.addEventListener("load", () => {
-        const loader = document.querySelector(".loader");
-        loader.classList.add("loader--hidden");
-        loader.addEventListener("transitionend", () => {
-            document.body.removeChild(loader);
-        });
-    });
+  
 </script>
 
 </html>
